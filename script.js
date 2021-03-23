534edec7a7ebe83e944a158a6fbb775e8e7dc1b2
Luke Teal <l.teal21@gmail.com>
2021-03-23 16:17:28 -0400

    Add Pseudo Coded solution in JS

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+
+// set arrays of possible values
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+
+  // prompt password length min = 8 max = 128
+    // if invalid value, alert/prompt for valid value
+    // if y, assign validated input to variable passLength
+  // prompt lowercase (Y or N)
+    // if invalid value, alert/prompt for valid value
+    // if y, push upper case array to passChar
+  // prompt uppercase (Y or N)
+    // if invalid value, alert/prompt for valid value
+    // if y, push uppercase array to passChar
+  // prompt numbers (Y or N)
+    // if invalid value, alert/prompt for valid value
+    // if y, push numbers array to passChar
+  // prompt special characters (Y or N)
+    // if invalid value, alert/prompt for valid value
+    // if y, push special characters array to passChar
+
+  // Validate that some type of character has been selected, if not, re-run questions
+
+  // pass passChar & passLength to function generatePassword and generate a password
+    // for the given length and available characters
+      // for (i=0, i<passLength.length, i++) {
+        // text += passChar[math.Floor]
+      }
 
   passwordText.value = password;
 
