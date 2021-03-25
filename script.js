505ab8d66d25d18a41bc2747c932195a1be0f417
Luke Teal <l.teal21@gmail.com>
2021-03-25 12:12:13 -0400

    add generate password function Also: make arrays constants clean up comments

--- script.js
+++ script.js
@@ -2,136 +2,115 @@
 const generateBtn = document.querySelector("#generate");
 
 // set arrays of possible values
-let charLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let charUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-let charNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-let charSpec = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', "`", '{', '|', '}', '~'];
+const charLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const charUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const charNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const charSpec = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', "`", '{', '|', '}', '~'];
+
 
 // Write password to the #password input
 function writePassword() {
-  // const password = generatePassword();
-  const passwordText = document.querySelector("#password");
-
+  
   let passChar = [];
 
-  // let passwordLength 
-
-  // for(let i=0; i < 8 || i > 128 || Number.isInteger(i)===false; i = passwordLength) {
-  //   passwordLength = prompt('Password Length?')
-  // }
-
+  // Loops for password length until full validated -
   let passLength
   while (passLength == null || passLength < 8 || passLength > 128 || !Number.isInteger(passLength)) {
     const questionLength = passLength == null ? 'Password length?' : 'Invalid password length, please enter a value between 8 and 128'
     passLength = parseFloat(prompt(questionLength, '8'))
   }
   
-  // Loop until full validated -
-    // Validate that password length is between 8 and 128
-    // let passworLength = 
-      // for(i=0, i>=8 && i<=128) {
-        // prompt('Password Length?')
-      // }
-      // prompt password length min = 8 max = 128
-        // if invalid value, alert/prompt for valid value
-        // if valid, assign validated input to variable passLength
 
-  // let passwordLength = prompt ('Password Length?')
 
-  // Loop until fully validated - 
-    // Validate that some type of character has been selected, if not, re-run questions
-    // for (i=0, i<1)
-      // prompt lowercase (Y or N)'
+  // Loops until at least one character type is input
   for (let i=0; i<1;) {
+    
+    // Loops until lowercase option is valid input - 
     let passLower
     while (passLower == null || (passLower !== 'Y' && passLower !== 'N')) {
       const questionLower = passLower == null ? 'Use Lower Case letters (Y/N)?' : 'Invalid response, please enter "Y" for yes and "N" for No. \nUse Lower Case letters?'
       passLower = prompt(questionLower, 'Y').toUpperCase()
+      
+      // if Y is chosen, increase i by 1 so the for loop knows that 
+      // at least one character type is chosen and 
+      // add lowercase characters to array of character options.
       if (passLower ==='Y') {
         i++;
         passChar = passChar.concat(charLower);
       }
     }
     
-        // if (prompt ('Use Lower Case letters (Y/N)?') === 'Y') {
-        //   passChar = passChar.concat(charLower);
-        // } 
-
-          // if invalid value, alert/prompt for valid value
-          // if y, push upper case array to passChar
-          // if y, add 1 to i
-        // prompt uppercase (Y or N)
-
+    // Loops until uppercase option is valid input - 
     let passUpper
     while (passUpper == null || (passUpper !== 'Y' && passUpper !== 'N')) {
       const questionUpper = passUpper == null ? 'Use Upper Case letters (Y/N)?' : 'Invalid response, please enter "Y" for yes and "N" for No. \nUse Upper Case letters?'
       passUpper = prompt(questionUpper, 'Y').toUpperCase()
+      
+      // if Y is chosen, increase i by 1 so the for loop knows that 
+      // at least one character type is chosen and 
+      // add uppercase characters to array of character options.
       if (passUpper ==='Y') {
         i++;
         passChar = passChar.concat(charUpper);
       }
-    }
-        // if (prompt ('Use Upper Case letters (Y/N)?') === 'Y') {
-        //   passChar = passChar.concat(charUpper);
-        // }
-          // if invalid value, alert/prompt for valid value
-          // if y, push uppercase array to passChar
-          // if y, add 1 to i
-        // prompt numbers (Y or N)
 
+    }
+
+    // Loops until numbers option is valid input - 
     let passNumber
     while (passNumber == null || (passNumber !== 'Y' && passNumber !== 'N')) {
       const questionNumber = passNumber == null ? 'Use Numbers (Y/N)?' : 'Invalid response, please enter "Y" for yes and "N" for No. \nUse Numbers?'
       passNumber = prompt(questionNumber, 'Y').toUpperCase()
+      
+      // if Y is chosen, increase i by 1 so the for loop knows that 
+      // at least one character type is chosen and 
+      // add numbers to array of character options.
       if (passNumber ==='Y') {
         i++;
         passChar = passChar.concat(charNum);
       }
+
     }
 
-        // if (prompt ('Use Numbers (Y/N)?') === 'Y') {
-        //   passChar = passChar.concat(charNum);
-        // }
-          // if invalid value, alert/prompt for valid value
-          // if y, push numbers array to passChar
-          // if y, add 1 to i
-        // prompt special characters (Y or N)
-        // prompt ('Use Special Characters (Y/N)?')
-
+      // Loops until special characters option is valid input - 
       let passSpecial
       while (passSpecial == null || (passSpecial !== 'Y' && passSpecial !== 'N')) {
         const questionSpecial = passSpecial == null ? 'Use Special Characters (Y/N)?' : 'Invalid response, please enter "Y" for yes and "N" for No. \nUse Special Characters?'
         passSpecial = prompt(questionSpecial, 'Y').toUpperCase()
+        
+        // if Y is chosen, increase i by 1 so the for loop knows that 
+        // at least one character type is chosen and 
+        // add special characters to array of character options.
         if (passSpecial ==='Y') {
           i++;
           passChar = passChar.concat(charSpec);
         }
       }
 
-        // if (prompt ('Use Special Characters (Y/N)?') === 'Y') {
-        //   passChar = passChar.concat(charSpec);
-        // }
-      
+      // alert user they did not select any options and loops back to beginning of character type questions
       if (i<1) {
         alert('You did not select any characer type.\nPlease select at least one character type.')
       }
     }
-      console.log(passChar)
 
-        // if invalid value, alert/prompt for valid value
-        // if y, push special characters array to passChar
-        // if y, add 1 to i
+  // for the given length and available characters, loop through each password position and 
+  // randomly choose a character from the character types chosen
+  function generatePassword() {
 
+    let text = "";
+    
+    for (i=0; i<passLength; i++) {
+      text += passChar[Math.floor(
+        Math.random() * passChar.length
+        )
+      ]
+    }
 
-  // pass passChar & passLength to function generatePassword and generate a password
-    // for the given length and available characters
-      // declare variable text: let text = "";
-      // for (i=0, i<passLength.length, i++) {
-        // text += passChar[Math.floor(
-          // Math.random() * passChar.length
-          // )
-        // }
+    return text;    
+  }
 
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
   passwordText.value = password;
 
 }
